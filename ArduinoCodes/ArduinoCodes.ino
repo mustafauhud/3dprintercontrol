b10d8938ceba66651211f1b35020593841b171fd
mustafauhud <uhud0823@gmail.com>
2023-05-31 00:02:43 +0300

    value index of

--- ArduinoCodes/ArduinoCodes.ino
+++ ArduinoCodes/ArduinoCodes.ino
@@ -3,20 +3,19 @@
 #include <SoftwareSerial.h>
 
 // Firebase bağlantısı için gerekli bilgiler
-#define FIREBASE_HOST "https://dprinter-control-default-rtdb.europe-west1.firebasedatabase.app/"
-#define FIREBASE_AUTH "26063e9e42d9cef3b3b6ce72b693b0b2c95b19c2"
+#define FIREBASE_HOST "https://nesnetez-5e48c-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "iJ1eUfdJdaXNvSUwfzFOhNgVYmu1"
 #define RX_PIN 13 //  pin on NodeMCU
 #define TX_PIN 15 //  pin on NodeMCU
 // WiFi ağı için gerekli bilgiler
-const char* WIFI_SSID = "Millenicom_9E6A";
-const char* WIFI_PASSWORD = "C0C9E3619E6A";
+const char* WIFI_SSID = "FiberHGW_TP1A26_2.4GHz";
+const char* WIFI_PASSWORD = "EvmTjjX7";
 SoftwareSerial rampsSerial(RX_PIN, TX_PIN);
 // Firebase nesnesi tanımlama
 FirebaseData firebaseData;
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
    rampsSerial.begin(115200);
 
   // WiFi ağına bağlanma
@@ -35,11 +34,39 @@ void setup() {
 }
 
 void loop() {
+  
    rampsSerial.println("M105");
-  Serial.println("nodemcu");
-  delay(2000);
+  delay(1500);
+   String receivedData = "";
+   while (rampsSerial.available() > 0) {  // Seri portta okunacak veri varsa
+      char character = rampsSerial.read();  // Veriyi oku
+      receivedData += character;  // Okunan karakteri String'e ekle
+    }
+    // T harfinin ve / işaretinin konumunu bul
+  int tIndex = receivedData.indexOf("T:");
+    int bIndex = receivedData.indexOf("B:");
+
+    if (tIndex >= 0 && bIndex >= 0) {  // "T:" ve "B:" bulundu mu kontrol et
+      // T: ve B: arasındaki 4 karakterli değeri al
+      String value = receivedData.substring(tIndex + 2, tIndex + 6);
+      String value1 = receivedData.substring(bIndex + 2, bIndex + 6);
+     // Alınan veriyi işle
+    // Örneğin, ekrana yazdırmak için:
+     double nozzle_temperature = value.toDouble();
+     double table_temperature = value1.toDouble();
+    Serial.print("Nozzle sicakligi: ");
+    Serial.println(nozzle_temperature);
+    Serial.print("Tabla sicakligi: ");
+    Serial.println(table_temperature);
+     Firebase.setDouble(firebaseData, "users/iJ1eUfdJdaXNvSUwfzFOhNgVYmu1/table_temperature", table_temperature);
+     Firebase.setDouble(firebaseData, "users/iJ1eUfdJdaXNvSUwfzFOhNgVYmu1/nozzle_temperature", nozzle_temperature);
+    delay(3000);
+    
+    }
+ 
+
   // Rastgele double değerler üretme
-  double  table_temperature = random(59, 62) ;
+ /* double  table_temperature = random(59, 62) ;
   double nozzle_temperature = random(204, 210) ;
   double x_position = random(0, 250) / 10.0;
   double y_position = random(0, 250) / 10.0;
@@ -49,15 +76,10 @@ void loop() {
   String string1 = "3D Printer Ready";
   
   
-  // Verileri Firebase'a gönderme
-  Firebase.setDouble(firebaseData, "example-data/table_temperature", table_temperature);
-  Firebase.setDouble(firebaseData, "example-data/nozzle_temperature", nozzle_temperature);
-  Firebase.setDouble(firebaseData, "example-data/x_position", x_position);
+  // Verileri Firebase'a gönderme*/
+  
+  /*Firebase.setDouble(firebaseData, "example-data/x_position", x_position);
   Firebase.setDouble(firebaseData, "example-data/y_position", y_position);
   Firebase.setDouble(firebaseData, "example-data/z_position", z_position);
-  delay(5000);
-  while (rampsSerial.available() > 0) {
-    char incomingByte = rampsSerial.read();
-    Serial.print(incomingByte); // Print the response to the computer's serial monitor
-  }
+  delay(5000);*/
  }
